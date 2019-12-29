f3f90dc0e0fb70c46f26312ba53d7fb3d9c3f9f2
Luc Nguyen <luc.nguyen@ut.edu.vn>
2019-12-29 17:02:18 +0700

    update

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,32 +1,26 @@
 import axios from 'axios';
 
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'Todo 1'
-        },
-        {
-            id: 2,
-            title: 'Todo 2'
-        },
-        {
-            id: 3,
-            title: 'Todo 3'
-        }
-    ],
+    todos: [],
 };
 
 const getters = {
     allTodos: (state) => state.todos
 };
 const actions = {
-    async fetchTodos({commit}) {
+    async fetchTodos({ commit }) {
         const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-        console.log(response.data);
+        commit('setTodos', response.data);
+    },
+    async addTodo({ commit }, title) {
+        const response = await axios.post('https://jsonplaceholder.typicode.com/todos', { title: title, completed: false });
+        commit('newTodo', response.data);
     }
 };
-const mutations = {};
+const mutations = {
+    setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, todo) => state.todos.unshift(todo),
+};
 
 export default {
     state,
